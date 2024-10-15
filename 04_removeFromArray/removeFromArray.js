b96c89cd15a5e0f756036c26658b533859e77693
Marc Fernand Dimacuha <themarcdimacuha@gmail.com>
2024-10-16 07:03:42 +0800

    Add solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,5 @@
 const removeFromArray = function(array, ...remove) {
-    const newArray = []
-    
-    for (let i = 0; i < remove.length; i++) {
-        for(let j = 0; j < array.length; j++) {
-            newArray.push(j)
-        }
-    }
-
-    return newArray
+    return array.filter(item => !remove.includes(item))
 };
 
 // Do not edit below this line
