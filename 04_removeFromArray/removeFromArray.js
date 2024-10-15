b92c3f59f39873aa284bcafcce51eb2a0229df83
Marc Fernand Dimacuha <themarcdimacuha@gmail.com>
2024-10-15 20:10:15 +0800

    Add basic structure to solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+    const newArray = []
+    
+    for (let i = 0; i < remove.length; i++) {
+        for(let j = 0; j < array.length; j++) {
+            newArray.push(j)
+        }
+    }
+
+    return newArray
 };
 
 // Do not edit below this line
