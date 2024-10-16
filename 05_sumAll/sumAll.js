8569c4af5dd036bad803299a545c887e4c5ff1f6
Marc Fernand Dimacuha <themarcdimacuha@gmail.com>
2024-10-16 09:38:47 +0800

    Add solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR"
+    } else if (typeof num1 === "string" || typeof num2 === "string") {
+        return "ERROR"
+    } else if (num1 % 1 !== 0 || num2 % 1 !== 0) {
+        return "ERROR"
+    }
+    
+    let sum = 0
+
+    if (num1 < num2) {
+        for (let i = num1; i <= num2; i++) {
+            sum += i
+        }
+    } else if (num1 > num2) {
+        for (let i = num2; i <= num1; i++) {
+            sum += i
+        }
+    }
+
+    return sum
+    
 };
 
 // Do not edit below this line
