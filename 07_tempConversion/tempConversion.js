ec846df88cb0f17e435009a14a1ac1bae446864b
Marc Fernand Dimacuha <themarcdimacuha@gmail.com>
2024-10-16 11:03:25 +0800

    Add solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(number) {
+  const result = (number - 32) * 5 / 9
+  return Math.round( result * 1e1 ) / 1e1;
 };
 
-const convertToFahrenheit = function() {
+
+
+const convertToFahrenheit = function(number) {
+  const result = (number * 9 / 5) + 32
+  return Math.round( result * 1e1 ) / 1e1;
 };
 
 // Do not edit below this line
